cfbe3c5a986c133b232cd0fb49707afd27b5c345
axrorback <axacyber1@gmail.com>
2025-09-17 23:47:52 +0500

    admin panel ozgardi urls

--- campus_events/urls.py
+++ campus_events/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('axrorback/', admin.site.urls),
     path('', include('events.urls')),
     path('users/', include('users.urls')),
     path('attendance/', include('attendance.urls')),
